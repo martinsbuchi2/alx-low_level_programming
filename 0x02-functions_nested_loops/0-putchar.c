e7c57ae5bb16c34293c8537e8d69c6dd31b3da10
martinsibuchim <martinsibuchim@gmail.com>
2023-02-22 02:58:43 -0800

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - print _putchar
- * is written by mbah
+ * is written by martinsUgwu
  * Return: 0
  */
 
@@ -18,4 +18,3 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
-:
