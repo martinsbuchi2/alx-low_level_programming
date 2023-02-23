598d1d805ee40c4f624bb694e8315891360556e0
martinsibuchim <martinsibuchim@gmail.com>
2023-02-23 10:23:28 -0800

    test for a postitive_or_negative() function

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
+
