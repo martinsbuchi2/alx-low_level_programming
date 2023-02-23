aa4a22e4f3e21070347d35860a0e9040af3c79b4
martinsibuchim <martinsibuchim@gmail.com>
2023-02-23 15:32:52 -0800

    print distance

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,6 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
 
 #endif
