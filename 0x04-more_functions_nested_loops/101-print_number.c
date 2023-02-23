2744359c72e998e70bff2f3e05068ea2a381946c
martinsibuchim <martinsibuchim@gmail.com>
2023-02-23 15:49:59 -0800

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -22,31 +22,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 
-	_putchar((n1 % 10) + '0');
-}
-
-
-
-
-101-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
+	_putchar((n1 % 10) + '0')
 }
