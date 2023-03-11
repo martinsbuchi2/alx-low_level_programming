69d70afafeb048eab1eced94c6312fd3e84720b8
martinsibuchim <martinsibuchim@gmail.com>
2023-03-11 03:01:55 -0800

    static library libmy.a containing all the listed functions

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,13 @@
 #include "main.h"
-#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-void _puts(char *s)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
