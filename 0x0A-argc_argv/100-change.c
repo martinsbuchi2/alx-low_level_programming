a9ee37afb90ce28508873b9ce3f591a7198b625c
martinsibuchim <martinsibuchim@gmail.com>
2023-03-10 21:07:25 -0800

    finds least number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,88 +1,81 @@
-#include <stdio.h>
-		
-#include <stdlib.h>
-		
 #include "main.h"
-		
+#include <stdio.h>
+#include <stdlib.h>
 
-		
 /**
-		
- * main - prints the minimum number of coins to
-		
- * make change for an amount of money
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: 0 (Success), 1 (Error)
-		
+ * make_change - finds least number of coins
+ * @n: money amount
+ * Return: number of coins
  */
-		
-int main(int argc, char *argv[])
-		
+
+int make_change(int n)
 {
-		
-	int num, j, result;
-		
-	int coins[] = {25, 10, 5, 2, 1};
-		
+	int coins, quarters, dimes, nickels, twocents, pennies;
 
-		
-	if (argc != 2)
-		
+	coins = 0;
+	quarters = 0;
+	dimes = 0;
+	nickels = 0;
+	twocents = 0;
+	pennies = 0;
+
+	while (n > 0)
 	{
-		
-		printf("Error\n");
-		
-		return (1);
-		
-	}
-		
-
-		
-	num = atoi(argv[1]);
-		
-	result = 0;
-		
-
-		
-	if (num < 0)
-		
-	{
-		
-		printf("0\n");
-		
-		return (0);
-		
-	}
-		
-
-		
-	for (j = 0; j < 5 && num >= 0; j++)
-		
-	{
-		
-		while (num >= coins[j])
-		
+		if (n >= 25)
 		{
-		
-			result++;
-		
-			num -= coins[j];
-		
+			n -= 25;
+			quarters += 1;
 		}
-		
+		else if (n >= 10)
+		{
+			n -= 10;
+			dimes += 1;
+		}
+		else if (n >= 5)
+		{
+			n -= 5;
+			nickels += 1;
+		}
+		else if (n >= 2)
+		{
+			n -= 2;
+			twocents += 1;
+		}
+		else
+		{
+			n -= 1;
+			pennies += 1;
+		}
 	}
-		
+	coins = quarters + dimes + nickels + twocents + pennies;
 
-		
-	printf("%d\n", result);
-		
-	return (0);
-		
+	return (coins);
 }
 
+/**
+ * main - prints minimum number of coins to make change
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		coins = make_change(atoi(argv[1]));
+		printf("%d\n", coins);
+	}
+	return (0);
+}
