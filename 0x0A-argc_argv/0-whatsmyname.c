22a309c86f74d071572d7856f47a004bc930c152
martinsibuchim <martinsibuchim@gmail.com>
2023-03-14 14:51:19 -0700

    function to print file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
-		
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - prints program name
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: (0)
+ * main - function to print file name
+ * @argc: count of the arguments supplied to the program and the
+ * @argv: an array of pointers to the strings
+ * Return: Always 0
  */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
