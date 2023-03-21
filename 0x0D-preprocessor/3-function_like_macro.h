273bebaada607dc61539e82461b0037fb616249d
martinsibuchim <martinsibuchim@gmail.com>
2023-03-21 14:22:09 -0700

    computes the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) ((x) < (0) ? -(x) : (x))
-
-#endif
+#define ABS(num) ((num) > (-1) ? (num) : (num * -1))
+#endif /* !FUNCTION_LIKE_MACRO_H */
