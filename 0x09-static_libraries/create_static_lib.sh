6860cb21abb837fc40a57568491a6a75d64a6d00
martinsibuchim <martinsibuchim@gmail.com>
2023-06-09 07:14:35 -0700

    static lib

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
+
+# Compile the .c files into object files
+gcc -c *.c
+
+# Create the static library
+ar rcs liball.a *.o
+
+# Clean up object files
+rm -f *.o
